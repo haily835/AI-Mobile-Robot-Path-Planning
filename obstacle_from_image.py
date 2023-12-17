2bf1dde0ee5d5b66528f1f4d918b4e0b83c3524d
Ly Hai <lyquochai835@gmail.com>
2023-12-16 23:02:24 -0500

    Add padding for obstacle

--- obstacle_from_image.py
+++ obstacle_from_image.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 from matplotlib.patches import Rectangle
 
 class Map:
-    def __init__(self, image_path, grid_size):
+    def __init__(self, image_path, grid_size, padding_size):
         # Load the black-and-white image
         self.image = plt.imread(image_path)
         self.flipped_image = np.flipud(self.image)
@@ -11,9 +12,12 @@ class Map:
         # Set the grid size
         self.grid_size = grid_size
 
+        # Set the padding size
+        self.padding_size = padding_size
+
         # Calculate the number of rows and columns in the grid
-        self.rows = self.image.shape[0] // self.grid_size
-        self.cols = self.image.shape[1] // self.grid_size
+        self.rows = math.ceil(self.image.shape[0] / self.grid_size)
+        self.cols = math.ceil(self.image.shape[1] / self.grid_size)
 
     def is_obstacle_in_grid(self, row, col):
         """
@@ -26,16 +30,23 @@ class Map:
         Returns:
         - True if the grid contains a black portion (obstacle), False otherwise.
         """
-        # Calculate the coordinates of the bottom-left corner of the grid
-        bottom_left_x = col * self.grid_size
-        bottom_left_y = (self.rows - row - 1) * self.grid_size  # Flip the row index
+        # Iterate over the surrounding grids with padding
+        for i in range(-self.padding_size, self.padding_size + 1):
+            for j in range(-self.padding_size, self.padding_size + 1):
+                # Calculate the coordinates of the bottom-left corner of the surrounding grid
+                surround_bottom_left_x = (col + j) * self.grid_size
+                surround_bottom_left_y = (self.rows - row - 1 - i) * self.grid_size  # Flip the row index
 
-        # Extract the region corresponding to the grid
-        grid_region = self.flipped_image[bottom_left_y:bottom_left_y + self.grid_size, bottom_left_x:bottom_left_x+self.grid_size]
+                # Check if the surrounding grid is within the image boundaries
+                if (surround_bottom_left_x >= 0 and surround_bottom_left_y >= 0 and
+                        surround_bottom_left_x + self.grid_size <= self.image.shape[1] and
+                        surround_bottom_left_y + self.grid_size <= self.image.shape[0]):
+                    # Check if any pixel in the surrounding grid is black (value = 0)
+                    if np.any(self.flipped_image[surround_bottom_left_y:surround_bottom_left_y+self.grid_size,
+                                                surround_bottom_left_x:surround_bottom_left_x+self.grid_size] == 0):
+                        return True  # If any surrounding grid contains an obstacle, consider the main grid as obstacle
 
-        # Check if any pixel in the grid is black (value = 0)
-        return np.any(grid_region == 0)
-
+        return False  # If none of the surrounding grids contains an obstacle, consider the main grid as obstacle
     def visualize_coordinate_system(self):
         """
         Visualize the coordinate system with labeled axes, mesh grid, obstacles, and arrows.
@@ -51,9 +62,10 @@ class Map:
             y = i * self.grid_size
             ax.axhline(y, color="black", linestyle="--", linewidth=0.5)
 
-
         ax.imshow(self.image, extent=[0, self.cols * self.grid_size, 0, self.rows * self.grid_size])
 
+        print(self.rows)
+        print(self.cols)
         # Draw obstacles
         for row in range(self.rows):
             for col in range(self.cols):
@@ -85,12 +97,14 @@ class Map:
 
         plt.show()
 
-# Example usage:
-image_path = "./obstacle.png"
-grid_size = 300
+if __name__ == "main":
+    # Example usage:
+    image_path = "./images/final_path.png"
+    grid_size = 25
+    padding_size = 2  # Set the desired padding size
 
-# Create a Map instance
-my_map = Map(image_path, grid_size)
+    # Create a Map instance
+    my_map = Map(image_path, grid_size, padding_size)
 
-# Visualize the coordinate system with labeled axes, mesh grid, obstacles, and arrows
-my_map.visualize_coordinate_system()
+    # Visualize the coordinate system with labeled axes, mesh grid, obstacles, and arrows
+    my_map.visualize_coordinate_system()
